e870b4cf714928532fb852a59740ea46017b5f40
tobiasvogel <tobiasvogel@users.noreply.github.com>
2023-01-19 16:35:53 +0100

    platform specific macros

--- application.h
+++ application.h
@@ -6,8 +6,21 @@
 #include <QtMultimedia/QAudioFormat>
 #include <QtMultimedia/QAudioOutput>
 #include <QtMath>
-#include "structs.h"
 
+#if defined(Q_OS_WIN32) || defined(Q_OS_WIN64) || defined(Q_OS_WIN) || defined(Q_OS_WINDOWS) || defined(Q_WS_WIN32)
+#include <windows.h>
+#include <winuser.h>
+
+//FIXME: include hook to catch Windows-key
+
+#endif
+
+
+#if defined(Q_OS_LINUX) || defined(Q_WS_X11)
+
+//FIXME: include hook to catch Meta/Super-key
+
+#endif
 
 class Application final : public QApplication {
 public:
