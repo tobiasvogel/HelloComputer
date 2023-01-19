677bb9f2328364ff82863bd12d8063c899662210
tobiasvogel <tobiasvogel@users.noreply.github.com>
2023-01-19 17:11:22 +0100

    windows key blocker

--- main.cpp
+++ main.cpp
@@ -5,6 +5,10 @@
 int main( int argc, char *argv[] ) {
    Application a( argc, argv );
    MainWindow w;
+   #if defined(Q_OS_WIN32) || defined(Q_OS_WIN64) || defined(Q_OS_WIN) || defined(Q_OS_WINDOWS) || defined(Q_WS_WIN32)
+     GlobalKeyBlocker blocker;
+     blocker.block();
+   #endif
    w.setWindowState( Qt::WindowFullScreen );
    w.show();
    return a.exec();
