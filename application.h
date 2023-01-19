90a0517e1c5d5eb914c137f44bd739be0c430286
tobiasvogel <tobiasvogel@users.noreply.github.com>
2023-01-19 17:10:42 +0100

    windows key blocker

--- application.h
+++ application.h
@@ -11,7 +11,39 @@
 #include <windows.h>
 #include <winuser.h>
 
-//FIXME: include hook to catch Windows-key
+
+class GlobalKeyBlocker {
+public:
+    GlobalKeyBlocker():mHKeyboardHook(NULL) {}
+    //to avoid leaking the hook procedure handle
+    ~GlobalKeyBlocker(){ unblock(); }
+    //hook callback function (called on every system-wide key press)
+    static LRESULT CALLBACK LowLevelKeyboardProc(int nCode,
+                                                 WPARAM wParam, LPARAM lParam) {
+        if(nCode == HC_ACTION) {
+            PKBDLLHOOKSTRUCT p = reinterpret_cast<PKBDLLHOOKSTRUCT>(lParam);
+            if(p->vkCode == VK_LWIN) return 1; //block left win key
+            if(p->vkCode == VK_RWIN) return 1; //block right win key
+        }
+        return CallNextHookEx(NULL, //ignored paramater
+                              nCode,
+                              wParam,
+                              lParam);
+    }
+    void block(){
+        mHKeyboardHook = SetWindowsHookEx(WH_KEYBOARD_LL, //low-level keyboard hool
+                                          &LowLevelKeyboardProc, //callback
+                                          GetModuleHandle(NULL),
+                                          0);
+    }
+    void unblock(){
+        if(mHKeyboardHook) UnhookWindowsHookEx(mHKeyboardHook);
+        mHKeyboardHook = NULL;
+    }
+
+private:
+    HHOOK mHKeyboardHook;
+};
 
 #endif
 
