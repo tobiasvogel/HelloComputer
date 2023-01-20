378e66fca86678bfeed963c30a2a1585000b9b92
Tobias X. Vogel <tobias.vogel@linux.com>
2023-01-20 15:59:31 +0100

    translations added

--- i18n/hellocomputer_de.ts
+++ i18n/hellocomputer_de.ts
@@ -2,24 +2,48 @@
 <!DOCTYPE TS>
 <TS version="2.1" language="de_DE">
 <context>
+    <name>Dialog</name>
+    <message>
+        <location filename="../settingsdialog.ui" line="32"/>
+        <location filename="../settingsdialog.ui" line="130"/>
+        <source>Einstellungen</source>
+        <translation>Einstellungen</translation>
+    </message>
+    <message>
+        <location filename="../settingsdialog.ui" line="142"/>
+        <source>Ton:</source>
+        <translation>Ton:</translation>
+    </message>
+    <message>
+        <location filename="../settingsdialog.ui" line="155"/>
+        <source>abspielen</source>
+        <translation>abspielen</translation>
+    </message>
+    <message>
+        <location filename="../settingsdialog.ui" line="171"/>
+        <source>Lautstärke:</source>
+        <translation>Lautstärke:</translation>
+    </message>
+    <message>
+        <location filename="../settingsdialog.ui" line="178"/>
+        <source>Farben:</source>
+        <translation>Farben:</translation>
+    </message>
+    <message>
+        <location filename="../settingsdialog.ui" line="314"/>
+        <source>&amp;Starten</source>
+        <translation>&amp;Starten</translation>
+    </message>
+    <message>
+        <location filename="../settingsdialog.ui" line="327"/>
+        <source>B&amp;eenden</source>
+        <translation>B&amp;eenden</translation>
+    </message>
+</context>
+<context>
     <name>MainWindow</name>
     <message>
-        <location filename="../mainwindow.ui" line="14"/>
-        <source>MainWindow</source>
-        <translation>MainWindow</translation>
-    </message>
-    <message>
-        <location filename="../mainwindow.ui" line="33"/>
-        <location filename="../mainwindow.ui" line="49"/>
-        <location filename="../mainwindow.ui" line="65"/>
-        <location filename="../mainwindow.ui" line="81"/>
-        <location filename="../mainwindow.ui" line="97"/>
-        <location filename="../mainwindow.ui" line="113"/>
-        <source>TextLabel</source>
-        <translation>TextLabel</translation>
-    </message>
-    <message>
-        <location filename="../mainwindow.cpp" line="20"/>
+        <location filename="../mainwindow.cpp" line="21"/>
         <source>Um das Programm zu beenden, drücken Sie die Tasten [%1] + [%2] + [%3] gleichzeitig.</source>
         <translation>Um das Programm zu beenden, drücken Sie die Tasten [%1] + [%2] + [%3] gleichzeitig.</translation>
     </message>
--- i18n/hellocomputer_en.ts
+++ i18n/hellocomputer_en.ts
@@ -2,24 +2,48 @@
 <!DOCTYPE TS>
 <TS version="2.1" language="en_US">
 <context>
+    <name>Dialog</name>
+    <message>
+        <location filename="../settingsdialog.ui" line="32"/>
+        <location filename="../settingsdialog.ui" line="130"/>
+        <source>Einstellungen</source>
+        <translation>Settings</translation>
+    </message>
+    <message>
+        <location filename="../settingsdialog.ui" line="142"/>
+        <source>Ton:</source>
+        <translation>Sound:</translation>
+    </message>
+    <message>
+        <location filename="../settingsdialog.ui" line="155"/>
+        <source>abspielen</source>
+        <translation>enable</translation>
+    </message>
+    <message>
+        <location filename="../settingsdialog.ui" line="171"/>
+        <source>Lautstärke:</source>
+        <translation>Volume:</translation>
+    </message>
+    <message>
+        <location filename="../settingsdialog.ui" line="178"/>
+        <source>Farben:</source>
+        <translation>Colors:</translation>
+    </message>
+    <message>
+        <location filename="../settingsdialog.ui" line="314"/>
+        <source>&amp;Starten</source>
+        <translation>&amp;Start</translation>
+    </message>
+    <message>
+        <location filename="../settingsdialog.ui" line="327"/>
+        <source>B&amp;eenden</source>
+        <translation>&amp;Exit</translation>
+    </message>
+</context>
+<context>
     <name>MainWindow</name>
     <message>
-        <location filename="../mainwindow.ui" line="14"/>
-        <source>MainWindow</source>
-        <translation>MainWindow</translation>
-    </message>
-    <message>
-        <location filename="../mainwindow.ui" line="33"/>
-        <location filename="../mainwindow.ui" line="49"/>
-        <location filename="../mainwindow.ui" line="65"/>
-        <location filename="../mainwindow.ui" line="81"/>
-        <location filename="../mainwindow.ui" line="97"/>
-        <location filename="../mainwindow.ui" line="113"/>
-        <source>TextLabel</source>
-        <translation>TextLabel</translation>
-    </message>
-    <message>
-        <location filename="../mainwindow.cpp" line="20"/>
+        <location filename="../mainwindow.cpp" line="21"/>
         <source>Um das Programm zu beenden, drücken Sie die Tasten [%1] + [%2] + [%3] gleichzeitig.</source>
         <translation>Press the [%1] + [%2] + [%3] keys at the same time to close the program.</translation>
     </message>
