736908675d60220ab1490c7787e73ad5d7afd0cb
tobiasvogel <tobiasvogel@users.noreply.github.com>
2023-01-22 15:00:36 +0100

    Update hellocomputer_de.ts

--- i18n/hellocomputer_de.ts
+++ i18n/hellocomputer_de.ts
@@ -5,37 +5,48 @@
     <name>Dialog</name>
     <message>
         <location filename="../settingsdialog.ui" line="32"/>
-        <location filename="../settingsdialog.ui" line="130"/>
+        <location filename="../settingsdialog.ui" line="97"/>
         <source>Einstellungen</source>
         <translation>Einstellungen</translation>
     </message>
     <message>
-        <location filename="../settingsdialog.ui" line="142"/>
+        <location filename="../settingsdialog.ui" line="109"/>
         <source>Ton:</source>
         <translation>Ton:</translation>
     </message>
     <message>
-        <location filename="../settingsdialog.ui" line="155"/>
+        <location filename="../settingsdialog.ui" line="122"/>
         <source>abspielen</source>
         <translation>abspielen</translation>
     </message>
     <message>
-        <location filename="../settingsdialog.ui" line="171"/>
+        <location filename="../settingsdialog.ui" line="138"/>
         <source>Lautstärke:</source>
         <translation>Lautstärke:</translation>
     </message>
     <message>
-        <location filename="../settingsdialog.ui" line="178"/>
+        <location filename="../settingsdialog.ui" line="208"/>
+        <source>Verzögerung zwischen zwei Farbwechseln:</source>
+        <translation>Verzögerung zwischen zwei Farbwechseln:</translation>
+    </message>
+    <message>
+        <location filename="../settingsdialog.ui" line="221"/>
+        <source> ms</source>
+        <extracomment>add a space before abbreviation of milliseconds</extracomment>
+        <translation> ms</translation>
+    </message>
+    <message>
+        <location filename="../settingsdialog.ui" line="272"/>
         <source>Farben:</source>
         <translation>Farben:</translation>
     </message>
     <message>
-        <location filename="../settingsdialog.ui" line="314"/>
+        <location filename="../settingsdialog.ui" line="329"/>
         <source>&amp;Starten</source>
         <translation>&amp;Starten</translation>
     </message>
     <message>
-        <location filename="../settingsdialog.ui" line="327"/>
+        <location filename="../settingsdialog.ui" line="342"/>
         <source>B&amp;eenden</source>
         <translation>B&amp;eenden</translation>
     </message>
@@ -43,9 +54,24 @@
 <context>
     <name>MainWindow</name>
     <message>
-        <location filename="../mainwindow.cpp" line="21"/>
+        <location filename="../mainwindow.cpp" line="23"/>
         <source>Um das Programm zu beenden, drücken Sie die Tasten [%1] + [%2] + [%3] gleichzeitig.</source>
         <translation>Um das Programm zu beenden, drücken Sie die Tasten [%1] + [%2] + [%3] gleichzeitig.</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="571"/>
+        <source>Komplett (%1 Farben)</source>
+        <translation>Komplett (%1 Farben)</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="572"/>
+        <source>Bunt (%1 Farben)</source>
+        <translation>Bunt (%1 Farben)</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="573"/>
+        <source>Pastell (%1 Farben</source>
+        <translation>Pastell (%1 Farben)</translation>
     </message>
 </context>
 </TS>
