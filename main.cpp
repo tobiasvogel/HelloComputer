d0d390902ed47420c43733425e885ba805295c84
tobiasvogel <tobiasvogel@users.noreply.github.com>
2023-01-23 14:36:47 +0100

    included icon-setting for linux

--- main.cpp
+++ main.cpp
@@ -1,6 +1,9 @@
 #include "application.h"
 #include "mainwindow.h"
 #include <QTranslator>
+#if defined(Q_OS_LINUX)
+#include <QFileInfo>
+#endif
 
 
 int main( int argc, char *argv[] ) {
@@ -12,6 +15,10 @@ int main( int argc, char *argv[] ) {
    #if defined(Q_OS_WIN32) || defined(Q_OS_WIN64) || defined(Q_OS_WIN) || defined(Q_OS_WINDOWS) || defined(Q_WS_WIN32) || defined(Q_OS_LINUX) || defined(Q_WS_X11)
    GlobalKeyBlocker blocker;
    blocker.block();
+   #endif
+   #if defined(Q_OS_LINUX)
+   QFileInfo fi(a.applicationDirPath() + "/../share/pixmaps/hellocomputer.png");
+   QGuiApplication::setWindowIcon(QIcon(fi.absoluteFilePath()));
    #endif
    w.setWindowState( Qt::WindowFullScreen );
    w.show();
